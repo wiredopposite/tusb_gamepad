bf3d362743519ac3ad30bc7a7b1b5e7d0dea963b
wiredopposite <jackxhorizon@gmail.com>
2024-04-16 15:11:03 -0600

    fix dinput bug

---
+++ drivermanager.cpp
@@ -0,0 +1,56 @@
+#include "usbd/drivermanager.h"
+
+#if (OGX_TYPE == WIRELESS) && (OGX_MCU != MCU_ESP32S3)
+#include "usbd/uartbridge/UARTBridgeDriver.h"
+#endif
+
+// #include "usbd/hid/HIDDriver.h"
+#include "usbd/dinput/DInputDriver.h"
+// #include "usbd/ps3/PS3Driver.h"
+
+#include "usbd/psclassic/PSClassicDriver.h"
+#include "usbd/switch/SwitchDriver.h"
+#include "usbd/xboxog/XboxOriginalDriver.h"
+#include "usbd/xinput/XInputDriver.h"
+#include "usbd/usbserial/USBSerialDriver.h"
+
+void DriverManager::setup(InputMode mode) 
+{
+    // driver = new DInputDriver();
+    switch (mode) 
+    {
+        case INPUT_MODE_HID:
+            // driver = new HIDDriver();
+            driver = new DInputDriver();
+            // driver = new PS3Driver();
+            break;
+        case INPUT_MODE_PSCLASSIC:
+            driver = new PSClassicDriver();
+            break;
+        case INPUT_MODE_SWITCH:
+            driver = new SwitchDriver();
+            break;
+        case INPUT_MODE_XBOXORIGINAL:
+            driver = new XboxOriginalDriver();
+            break;
+        case INPUT_MODE_XINPUT:
+            driver = new XInputDriver();
+            break;
+        case INPUT_MODE_USBSERIAL:
+            driver = new USBSerialDriver();
+            break;
+        #if (OGX_TYPE == WIRELESS) && (OGX_MCU != MCU_ESP32S3)
+        case INPUT_MODE_UART_BRIDGE:
+            driver = new UARTBridgeDriver();
+            break;
+        #endif
+        default:
+            return;
+    }
+    
+    // Initialize our chosen driver
+    driver->initialize();
+
+    // Start the TinyUSB Device functionality
+    tud_init(TUD_OPT_RHPORT);
+}
